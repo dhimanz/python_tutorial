3c804e0be4982189e54c9f2c7358c58a2be1a7a1
Keshav Dhiman <dhimaansaab4130@gmail.com>
2026-03-03 10:03:03 +0530

    decimal to hexadecimal

--- main.py
+++ main.py
@@ -6,3 +6,10 @@
 #         return num[0]+sum(num[1:])
 #
 # print(sum([1,3]))                 [sum of list]
+# def to_string(n,base):
+#     converted="0123456789ABCDEF"
+#     if n<base:
+#         return converted[n]
+#     else:
+#         return to_string(n//base,base)+converted[n%base]
+# print(to_string(2835,16))         [decimal to hexadecimal
